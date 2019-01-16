340392d9f9c31489bb34b70a8bb31f34623e4c9a
anandhupvr <anandhupvr@gmail.com>
2019-01-16 10:32:37 +0000

    skeleton

---
+++ trainer.py
@@ -0,0 +1,295 @@
+from data.loader import LOAD
+from configs.config import Config
+import os
+import random
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from models.network import NET
+import tensorflow as tf
+from keras import backend as K
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Flatten, Dense, Input, Conv2D, MaxPooling2D, Dropout, Concatenate
+
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, TimeDistributed
+from keras.engine.topology import get_source_inputs
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.objectives import categorical_crossentropy
+
+from keras.models import Model
+from keras.utils import generic_utils
+from keras.engine import Layer, InputSpec
+from keras import initializers, regularizers
+import losses.loss as losses
+import lib.utils as utils
+
+net = NET()
+C = Config()
+load = LOAD()
+
+base_path = ''
+train_path = ''
+
+horizontal_flips = True
+vertical_flips = True
+rot_90 = True
+
+train_imgs, classes_count, class_mapping = load.get_data("annotation.txt")
+class_mapping.update({'bg':1})
+
+output_weight_path = os.path.join(base_path, 'model/model_frcnn_vgg.hdf5')
+record_path = os.path.join(base_path, 'model/record.csv') # Record data (used to save the losses, classification accuracy and mean average precision)
+base_weight_path = os.path.join(base_path, 'model/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+config_output_filename = os.path.join(base_path, 'model_vgg_config.pickle')
+
+
+C.use_horizontal_flips = horizontal_flips
+C.use_vertical_flips = vertical_flips
+C.rot_90 = rot_90
+num_rois = 4
+
+C.record_path = record_path
+C.model_path = output_weight_path
+C.num_rois = num_rois
+
+C.base_net_weights = base_weight_path
+
+
+C.class_mapping = class_mapping
+def get_img_output_length(width, height):
+	return (int(width/16),int(height/16)) 
+
+random.seed(1)
+random.shuffle(train_imgs)
+data_gen_train = load.get_anchor_gt(train_imgs, C, get_img_output_length, mode='train')
+
+
+# X, Y, image_data, debug_img, debug_num_pos = next(data_gen_train)
+
+# print('Original image: height=%d width=%d'%(image_data['height'], image_data['width']))
+# print('Resized image:  height=%d width=%d C.im_size=%d'%(X.shape[1], X.shape[2], C.im_size))
+# print('Feature map size: height=%d width=%d C.rpn_stride=%d'%(Y[0].shape[1], Y[0].shape[2], C.rpn_stride))
+# print(X.shape)
+# print(str(len(Y))+" includes 'y_rpn_cls' and 'y_rpn_regr'")
+# print('Shape of y_rpn_cls {}'.format(Y[0].shape))
+# print('Shape of y_rpn_regr {}'.format(Y[1].shape))
+# print(image_data)
+'''
+print('Number of positive anchors for this image: %d' % (debug_num_pos))
+if debug_num_pos==0:
+    gt_x1, gt_x2 = image_data['bboxes'][0]['x1']*(X.shape[2]/image_data['height']), image_data['bboxes'][0]['x2']*(X.shape[2]/image_data['height'])
+    gt_y1, gt_y2 = image_data['bboxes'][0]['y1']*(X.shape[1]/image_data['width']), image_data['bboxes'][0]['y2']*(X.shape[1]/image_data['width'])
+    gt_x1, gt_y1, gt_x2, gt_y2 = int(gt_x1), int(gt_y1), int(gt_x2), int(gt_y2)
+
+    img = debug_img.copy()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    color = (0, 255, 0)
+    cv2.putText(img, 'gt bbox', (gt_x1, gt_y1-5), cv2.FONT_HERSHEY_DUPLEX, 0.7, color, 1)
+    cv2.rectangle(img, (gt_x1, gt_y1), (gt_x2, gt_y2), color, 2)
+    cv2.circle(img, (int((gt_x1+gt_x2)/2), int((gt_y1+gt_y2)/2)), 3, color, -1)
+
+    plt.grid()
+    plt.imshow(img)
+    plt.show()
+else:
+    cls = Y[0][0]
+    pos_cls = np.where(cls==1)
+    print(pos_cls)
+    regr = Y[1][0]
+    pos_regr = np.where(regr==1)
+    print(pos_regr)
+    print('y_rpn_cls for possible pos anchor: {}'.format(cls[pos_cls[0][0],pos_cls[1][0],:]))
+    print('y_rpn_regr for positive anchor: {}'.format(regr[pos_regr[0][0],pos_regr[1][0],:]))
+
+    gt_x1, gt_x2 = image_data['bboxes'][0]['x1']*(X.shape[2]/image_data['width']), image_data['bboxes'][0]['x2']*(X.shape[2]/image_data['width'])
+    gt_y1, gt_y2 = image_data['bboxes'][0]['y1']*(X.shape[1]/image_data['height']), image_data['bboxes'][0]['y2']*(X.shape[1]/image_data['height'])
+    gt_x1, gt_y1, gt_x2, gt_y2 = int(gt_x1), int(gt_y1), int(gt_x2), int(gt_y2)
+
+    img = debug_img.copy()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    color = (0, 255, 0)
+    #   cv2.putText(img, 'gt bbox', (gt_x1, gt_y1-5), cv2.FONT_HERSHEY_DUPLEX, 0.7, color, 1)
+    cv2.rectangle(img, (gt_x1, gt_y1), (gt_x2, gt_y2), color, 2)
+    cv2.circle(img, (int((gt_x1+gt_x2)/2), int((gt_y1+gt_y2)/2)), 3, color, -1)
+
+    # Add text
+    textLabel = 'gt bbox'
+    (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,0.5,1)
+    textOrg = (gt_x1, gt_y1+5)
+    cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
+    cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
+    cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1)
+
+    # Draw positive anchors according to the y_rpn_regr
+    for i in range(debug_num_pos):
+
+        color = (100+i*(155/4), 0, 100+i*(155/4))
+
+        idx = pos_regr[2][i*4]/4
+        anchor_size = C.anchor_box_scales[int(idx/3)]
+        anchor_ratio = C.anchor_box_ratios[2-int((idx+1)%3)]
+
+        center = (pos_regr[1][i*4]*C.rpn_stride, pos_regr[0][i*4]*C.rpn_stride)
+        print('Center position of positive anchor: ', center)
+        cv2.circle(img, center, 3, color, -1)
+        anc_w, anc_h = anchor_size*anchor_ratio[0], anchor_size*anchor_ratio[1]
+        cv2.rectangle(img, (center[0]-int(anc_w/2), center[1]-int(anc_h/2)), (center[0]+int(anc_w/2), center[1]+int(anc_h/2)), color, 2)
+#         cv2.putText(img, 'pos anchor bbox '+str(i+1), (center[0]-int(anc_w/2), center[1]-int(anc_h/2)-5), cv2.FONT_HERSHEY_DUPLEX, 0.5, color, 1)
+
+print('Green bboxes is ground-truth bbox. Others are positive anchors')
+plt.figure(figsize=(8,8))
+plt.grid()
+plt.imshow(img)
+plt.show()
+'''
+
+input_shape_img = (None, None, 3)
+
+img_input = Input(shape=input_shape_img)
+roi_input = Input(shape=(None, 4))
+
+shared_layers = net.nn_base(img_input, trainable=True)
+num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios) # 9
+
+rpn = net.rpn_layer(shared_layers, num_anchors)
+# roi = ROI(shared_layers)
+
+
+classifier = net.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count)+1, trainable=True)
+
+
+model_rpn = Model(img_input, rpn[:2])
+model_classifier = Model([img_input, roi_input], classifier)
+
+model_all = Model([img_input, roi_input], rpn[:2] + classifier)
+
+
+optimizer = Adam(lr=1e-5)
+optimizer_classifier = Adam(lr=1e-5)
+model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count))], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_all.compile(optimizer='sgd', loss='mae')
+
+
+epoch_length = 1000
+num_epochs = 100
+iter_num = 0
+
+losses = np.zeros((epoch_length, 5))
+rpn_accuracy_rpn_monitor = []
+rpn_accuracy_for_epoch = []
+
+best_loss = np.Inf
+
+class_mapping_inv = {v: k for k, v in class_mapping.items()}
+print('Starting training')
+
+vis = True
+for epoch_num in range(num_epochs):
+
+    progbar = generic_utils.Progbar(epoch_length)
+    print('Epoch {}/{}'.format(epoch_num + 1, num_epochs))
+
+    while True:
+
+        if len(rpn_accuracy_rpn_monitor) == epoch_length and C.verbose:
+            mean_overlapping_bboxes = float(sum(rpn_accuracy_rpn_monitor))/len(rpn_accuracy_rpn_monitor)
+            rpn_accuracy_rpn_monitor = []
+            print('Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(mean_overlapping_bboxes, epoch_length))
+            if mean_overlapping_bboxes == 0:
+                print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
+
+        X, Y, img_data, debug_img, debug_num_pos = next(data_gen_train)
+
+        loss_rpn = model_rpn.train_on_batch(X, Y)
+
+        P_rpn = model_rpn.predict_on_batch(X)
+        R = utils.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+        # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
+        X2, Y1, Y2, IouS = utils.calc_iou(R, img_data, C, class_mapping)
+
+        if X2 is None:
+            rpn_accuracy_rpn_monitor.append(0)
+            rpn_accuracy_for_epoch.append(0)
+            continue
+
+        neg_samples = np.where(Y1[0, :, -1] == 1)
+        pos_samples = np.where(Y1[0, :, -1] == 0)
+
+        if len(neg_samples) > 0:
+            neg_samples = neg_samples[0]
+        else:
+            neg_samples = []
+
+        if len(pos_samples) > 0:
+            pos_samples = pos_samples[0]
+        else:
+            pos_samples = []
+        
+        rpn_accuracy_rpn_monitor.append(len(pos_samples))
+        rpn_accuracy_for_epoch.append((len(pos_samples)))
+        if C.num_rois > 1:
+            if len(pos_samples) < C.num_rois//2:
+                selected_pos_samples = pos_samples.tolist()
+            else:
+                selected_pos_samples = np.random.choice(pos_samples, C.num_rois//2, replace=False).tolist()
+            try:
+                selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
+            except:
+                selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=True).tolist()
+
+            sel_samples = selected_pos_samples + selected_neg_samples
+        else:
+            # in the extreme case where num_rois = 1, we pick a random pos or neg sample
+            selected_pos_samples = pos_samples.tolist()
+            selected_neg_samples = neg_samples.tolist()
+            if np.random.randint(0, 2):
+                sel_samples = random.choice(neg_samples)
+            else:
+                sel_samples = random.choice(pos_samples)
+
+
+        loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+
+        losses[iter_num, 0] = loss_rpn[1]
+        losses[iter_num, 1] = loss_rpn[2]
+
+        losses[iter_num, 2] = loss_class[0]
+        losses[iter_num, 3] = loss_class[1]
+        losses[iter_num, 4] = loss_class[2  ]
+
+        iter_num += 1
+
+        progbar.update(iter_num, [('rpn_cls', np.mean(losses[:iter_num, 0])), ('rpn_regr', np.mean(losses[:iter_num, 1])),
+                                  ('detector_cls', np.mean(losses[:iter_num, 2])), ('detector_regr', np.mean(losses[:iter_num, 3]))])
+
+        if iter_num == epoch_length:
+            loss_rpn_cls = np.mean(losses[:, 0])
+            loss_rpn_regr = np.mean(losses[:, 1])
+            loss_class_cls = np.mean(losses[:, 2])
+            loss_class_regr = np.mean(losses[:, 3])
+            class_acc = np.mean(losses[:, 4])
+
+            mean_overlapping_bboxes = float(sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
+            rpn_accuracy_for_epoch = []
+
+            if C.verbose:
+                print('Mean number of bounding boxes from RPN overlapping ground truth boxes: {}'.format(mean_overlapping_bboxes))
+                print('Classifier accuracy for bounding boxes from RPN: {}'.format(class_acc))
+                print('Loss RPN classifier: {}'.format(loss_rpn_cls))
+                print('Loss RPN regression: {}'.format(loss_rpn_regr))
+                print('Loss Detector classifier: {}'.format(loss_class_cls))
+                print('Loss Detector regression: {}'.format(loss_class_regr))
+                print('Elapsed time: {}'.format(time.time() - start_time))
+
+            curr_loss = loss_rpn_cls + loss_rpn_regr + loss_class_cls + loss_class_regr
+            iter_num = 0
+            start_time = time.time()
+
+            if curr_loss < best_loss:
+                if C.verbose:
+                    print('Total loss decreased from {} to {}, saving weights'.format(best_loss,curr_loss))
+                best_loss = curr_loss
+                model_all.save_weights(C.model_path)
+print('Training complete, exiting.')
