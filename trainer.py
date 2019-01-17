547dd0554160473871abb8926ec679b3e967b3f2
anandhupvr <anandhupvr@gmail.com>
2019-01-17 06:08:27 +0000

    tensorboard

--- trainer.py
+++ trainer.py
@@ -190,6 +190,12 @@ tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
 
 tensorboard.set_model(model_all)
 
+def named_logs(model, logs):
+    result = {}
+    for l in zip(model.metrics_names, logs):
+    result[l[0]] = l[1]
+    return result
+
 vis = True
 for epoch_num in range(num_epochs):
 
